4734866f3ed45f25800d71d19fb6d150d3cf4770
Otman Karim <71219931+Ott00@users.noreply.github.com>
2023-12-01 10:09:54 +0100

    create components, service and interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoPageComponent } from './components/todo-page/todo-page.component';
+import { CompletedPageComponent } from './components/completed-page/completed-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoPageComponent,
+    CompletedPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/completed-page/completed-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-completed-page',
+  templateUrl: './completed-page.component.html',
+  styleUrls: ['./completed-page.component.scss']
+})
+export class CompletedPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
