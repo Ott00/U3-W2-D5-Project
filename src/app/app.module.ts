dfa77c14df0d51eb7a1295ab6a3470efb310b5f2
Otman Karim <71219931+Ott00@users.noreply.github.com>
2023-12-01 10:38:43 +0100

    added routing and ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { TodoPageComponent } from './components/todo-page/todo-page.component';
 import { CompletedPageComponent } from './components/completed-page/completed-page.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+
+const routes: Route[] = [
+  {
+    path: 'todos',
+    component: TodoPageComponent,
+  },
+  {
+    path: 'completed',
+    component: CompletedPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'todos',
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoPageComponent,
     CompletedPageComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
